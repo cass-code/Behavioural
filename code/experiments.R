e20a06ad4e521461469f37211117524ca32312d5
cass-code <20346212@sun.ac.za>
2021-10-14 05:06:32 +0200

    updated lit review

--- code/experiments.R
+++ code/experiments.R
@@ -1,9 +1,9 @@
 experiments <- function(){
 
 
-Group <- c("Control", "Group 1", "Group 2", "Group 3")
+Group <- c("Control", "Group 1", "Group 2", "", "Group 3")
 #Treatment <- c("No lottery", "Standard Lottery", "Regret Lottery", "Referral Lottery")
-Treatment <- c("No lottery", "Individual is entered into a lottery once they are vaccinated", "Everyone in the group is entered into a lottery; only vaccinated individuals can claim the prize", "Individual is entered if she is vaccinated and refers a friend, who gets vaccinated")
+Treatment <- c("No lottery", "Individual is entered into a lottery once they are vaccinated", "Everyone in the group is entered into a lottery; only vaccinated individuals can", "claim the prize", "Individual is entered if she is vaccinated and refers a friend, who gets vaccinated")
 frame <- data.frame(Group, Treatment)
 frame
 
